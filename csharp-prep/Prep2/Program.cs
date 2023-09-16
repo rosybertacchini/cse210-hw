6683a7d454987c8f523bdb58423332e5a7d59892
rosybertacchini <rosybertacchini@gmail.com>
2023-09-16 06:29:09 -0700

    Conditions

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        string letter = "";
+        Console.Write("What is you grade percentage? ");
+        string InputGradePercentage = Console.ReadLine();
+        int gradePercentage = int.Parse(InputGradePercentage);
+        if (gradePercentage >= 90 ) {
+                letter = "A";
+        } else if (gradePercentage >= 80 ) {
+                    letter = "B";
+            }  else if (gradePercentage >= 70  ) {
+                        letter = "C";
+                }  else if (gradePercentage >= 60  ) {
+                             letter = "D";
+                     }  else {
+                                letter = "F";
+                         } 
+        Console.WriteLine($"You grade is {letter}");
+
+        if (gradePercentage >= 70) {
+                Console.WriteLine("Concratulations... you pass the class");
+        } else { 
+                Console.WriteLine("You did not pass the class... courage, keep going!");
+             }
     }
 }
